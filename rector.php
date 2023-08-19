df67a46bd2b5a753abfd81989736c910704ece9c
Nuno Maduro <enunomaduro@gmail.com>
2023-08-19 16:11:10 +0100

    chore: refactor routes

--- rector.php
+++ rector.php
@@ -14,6 +14,7 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__.'/config',
         __DIR__.'/database',
         __DIR__.'/public',
+        __DIR__.'/routes',
     ]);
 
     $rectorConfig->rules([
